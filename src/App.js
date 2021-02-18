6dfc21593c16dadec83b2c9a2d7c30bf0843fba6
shazy89 <erdo.shazy123@gmail.com>
2021-02-17 21:44:29 -0500

    css updates

--- src/App.js
+++ src/App.js
@@ -7,34 +7,34 @@ const App = () => {
         <div className="digit-screen box"></div>
 
         <div className="buttons-row">
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4">c</button>
+          <button className="btn pure-u-1-4">+/-</button>
+          <button className="btn pure-u-1-4">%</button>
+          <button className="btn pure-u-1-4">%</button>
         </div>
         <div className="buttons-row">
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4">1</button>
+          <button className="btn pure-u-1-4">2</button>
+          <button className="btn pure-u-1-4">3</button>
+          <button className="btn pure-u-1-4">*</button>
         </div>
         <div className="buttons-row">
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4">4</button>
+          <button className="btn pure-u-1-4">5</button>
+          <button className="btn pure-u-1-4">6</button>
+          <button className="btn pure-u-1-4">-</button>
         </div>
         <div className="buttons-row">
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
-          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4">7</button>
+          <button className="btn pure-u-1-4">8</button>
+          <button className="btn pure-u-1-4">9</button>
+          <button className="btn pure-u-1-4">+</button>
         </div>
 
         <div className="buttons-row">
-          <button className="btn pure-u-1-3"></button>
-          <button className="btn pure-u-1-3"></button>
-          <button className="btn-zero pure-u-1-3"></button>
+          <button className="btn-zero pure-u-1-3">0</button>
+          <button className="btn pure-u-1-3">,</button>
+          <button className="btn pure-u-1-3">=</button>
         </div>
       </div>
     </div>
