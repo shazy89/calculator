6875d3c38f039c1935d54bd00092c8124a378131
shazy89 <erdo.shazy123@gmail.com>
2021-02-17 21:32:57 -0500

    add style to buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,37 @@ const App = () => {
     <div className="intro">
       <div className="calculator box">
         <div className="digit-screen box"></div>
+
+        <div className="buttons-row">
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+        </div>
+        <div className="buttons-row">
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+        </div>
+        <div className="buttons-row">
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+        </div>
+        <div className="buttons-row">
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+          <button className="btn pure-u-1-4"></button>
+        </div>
+
+        <div className="buttons-row">
+          <button className="btn pure-u-1-3"></button>
+          <button className="btn pure-u-1-3"></button>
+          <button className="btn-zero pure-u-1-3"></button>
+        </div>
       </div>
     </div>
   );
