de5d6a505dff0c2c771df213eda9d70574330905
shazy89 <erdo.shazy123@gmail.com>
2021-02-17 20:26:27 -0500

    calculator

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>HELLO WORLD</h1>
+    <div className="intro">
+      <div className="calculator"></div>
     </div>
   );
 }
